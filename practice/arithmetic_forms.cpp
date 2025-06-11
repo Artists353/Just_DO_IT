475d4723a923aeb841d89b2a83ee88248d26778b
Artists <arists228.grogy@gmail.com>
2025-06-11 20:36:42 +0300

    arithmetic_forms
    Аремфитические формы

---
+++ practice/arithmetic_forms.cpp
@@ -0,0 +1,17 @@
+//Подключаем базовую библиотеку
+#include <iostream>
+using namespace std; 
+
+int main(){
+    int a = 0;
+
+    cout << a << endl;
+
+    a = a + 20;
+
+    cout << a << endl;
+
+    a += 20;
+
+    cout << a << endl;
+}
